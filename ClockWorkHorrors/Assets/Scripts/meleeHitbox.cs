b1e91c85cafa56eb59c5b31eac9cb5c9841073a7
JacobBragg <jabragg@student.fullsail.edu>
2025-04-15 22:30:56 -0400

    Melee Hitbox
    The enemy can have a hitbox placed on them to trigger a melee attack

---
+++ ClockWorkHorrors/Assets/Scripts/meleeHitbox.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class meleeHitbox : MonoBehaviour
+{
+    [SerializeField] int damageAmount;
+    [SerializeField] float activeTime = 0.3f;
+
+    bool alreadyHit;
+
+    void OnEnable()
+    {
+        alreadyHit = false;
+        Invoke(nameof(DisableHitbox), activeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (alreadyHit || other.isTrigger)
+            return;
+
+        IDamage dmg = other.GetComponent<IDamage>();
+        if (dmg != null)
+        {
+            dmg.takeDamage(damageAmount);
+            alreadyHit = true;
+        }
+    }
+
+    void DisableHitbox()
+    {
+        gameObject.SetActive(false);
+    }
+}
